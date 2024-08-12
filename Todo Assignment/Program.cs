273175c484ca6708f3f6738e31e9bedf38387402
elvisonob <elvisonob@yahoo.com>
2024-08-12 18:38:18 +0100

    Add project files.

---
+++ Todo Assignment/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do");
+Console.WriteLine("[S]ee all TODOS" );
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+
+string userType = Console.ReadLine();
+List<string> todos = new List<string>();
+
+do
+{
+    
+
+    if (userType == "Aa" || userType == "a")
+    {
+        Console.WriteLine("what do you want to add");
+        var addTodos = Console.ReadLine();
+        
+    }
+
+     if (userType == "S" || userType == "s")
+    {
+
+        foreach (var i in todos)
+        {
+            Console.WriteLine(i);
+        }
+
+    }
+
+     if (userType == "R" || userType == "r")
+    {
+        //remove a unique todos
+        foreach (var i in todos)
+        {
+            todos.Remove(i);
+        }
+
+    }
+} while (userType != "E" || userType != "e");
+
+
+
+
