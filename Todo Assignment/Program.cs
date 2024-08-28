6888d447619cf7c3c647d0380106265b8685c6d9
elvisonob <elvisonob@yahoo.com>
2024-08-28 19:41:23 +0100

    Implementing the todos

--- Todo Assignment/Program.cs
+++ Todo Assignment/Program.cs
@@ -1,34 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello!");
-Console.WriteLine("What do you want to do");
-Console.WriteLine("[S]ee all TODOS" );
-Console.WriteLine("[A]dd a TODO");
-Console.WriteLine("[R]emove a TODO");
-Console.WriteLine("[E]xit");
+
+bool shallExit = false;
+var todos = new List<string>();
+
+while (!shallExit)
+{
+    Console.WriteLine("Hello!");
+    Console.WriteLine("What do you want to do");
+    Console.WriteLine("[S]ee all TODOS");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
 
 
-string userType = Console.ReadLine();
- var todos = new List<string> { "studying Context", "read on the bus", "go to work"};
-
+    string userType = Console.ReadLine();
+  
 
     switch (userType)
     {
         case "E":
         case "e":
             Console.WriteLine("Exit");
+            shallExit = true;
             break;
 
-    case "A":
-    case "a":
-        Console.WriteLine("Add a todo");
-        if (userType == "")
-        {
-            Console.WriteLine("User input cannot be empty");
-        }
-        todos.Add(userType);
-        break;
+        case "A":
+        case "a":
+            bool isDescriptionValid = false;
+            while (!isDescriptionValid)
+            {
+                Console.WriteLine("Add a todo");
+                var description = Console.ReadLine();
+                if (description == "")
+                {
+                    Console.WriteLine("User input cannot be empty");
+                }
+                else if (todos.Contains(description))
+                {
+                    Console.WriteLine("description must be unique");
+                }
+                else
+                {
+                    isDescriptionValid = true;
+                    todos.Add(description);
+                }
+            }
+            break;
 
-    case "S":
+        case "S":
         case "s":
             for (var i = 0; i < todos.Count; ++i)
             {
@@ -38,26 +57,33 @@ string userType = Console.ReadLine();
 
         case "R":
         case "r":
-            // when a word is typed, and the word is not in the todosList, it should 
-            // trigger, BUT if it is in the todos list, it should be removed
-            Console.WriteLine("Add a todo");
+
+            var remove = Console.ReadLine();
             for (var i = 0; i < todos.Count; ++i)
             {
-                if (!userType.Contains(todos[i]))
+                remove = todos[i];
+                if (!todos.Contains(remove))
                 {
                     Console.WriteLine("We can't remove what does not exist");
                 }
-
-                todos.Remove(todos[i]);
-
+                else
+                {
+                    todos.Remove(remove);
+                }
             }
             break;
 
-    default:
-        Console.WriteLine("Invalid Choice");
-        break;
+        default:
+            Console.WriteLine("Invalid Choice");
+            break;
 
     }
+}
+
+Console.ReadKey();
+
+
+
 
     
  
