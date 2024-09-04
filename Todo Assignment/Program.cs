3aece2cf0db6b045ea818bd49cbb2cf15805704c
elvisonob <elvisonob@yahoo.com>
2024-09-04 18:56:27 +0100

    Out method

--- Todo Assignment/Program.cs
+++ Todo Assignment/Program.cs
@@ -1,83 +1,106 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-bool shallExit = false;
-var todos = new List<string>();
+//bool shallExit = false;
+//var todos = new List<string>();
 
-while (!shallExit)
+//while (!shallExit)
+//{
+//    Console.WriteLine("Hello!");
+//    Console.WriteLine("What do you want to do");
+//    Console.WriteLine("[S]ee all TODOS");
+//    Console.WriteLine("[A]dd a TODO");
+//    Console.WriteLine("[R]emove a TODO");
+//    Console.WriteLine("[E]xit");
+
+
+//    string userType = Console.ReadLine();
+
+
+//    switch (userType)
+//    {
+//        case "E":
+//        case "e":
+//            Console.WriteLine("Exit");
+//            shallExit = true;
+//            break;
+
+//        case "A":
+//        case "a":
+//            bool isDescriptionValid = false;
+//            while (!isDescriptionValid)
+//            {
+//                Console.WriteLine("Add a todo");
+//                var description = Console.ReadLine();
+//                if (description == "")
+//                {
+//                    Console.WriteLine("User input cannot be empty");
+//                }
+//                else if (todos.Contains(description))
+//                {
+//                    Console.WriteLine("description must be unique");
+//                }
+//                else
+//                {
+//                    isDescriptionValid = true;
+//                    todos.Add(description);
+//                }
+//            }
+//            break;
+
+//        case "S":
+//        case "s":
+//            for (var i = 0; i < todos.Count; ++i)
+//            {
+//                Console.WriteLine(todos[i]);
+//            }
+//            break;
+
+//        case "R":
+//        case "r":
+
+//            var remove = Console.ReadLine();
+//            for (var i = 0; i < todos.Count; ++i)
+//            {
+//                remove = todos[i];
+//                if (!todos.Contains(remove))
+//                {
+//                    Console.WriteLine("we can't remove what does not exist");
+//                }
+//                else
+//                {
+//                    todos.Remove(remove);
+//                }
+//            }
+//            break;
+
+//        default:
+//            Console.WriteLine("Invalid Choice");
+//            break;
+
+//    }
+//}
+
+var numbers = new[] { 10, -8, 2, 12, -17 };
+
+List<int> GetPositiveNumbers(int[] numbers)
 {
-    Console.WriteLine("Hello!");
-    Console.WriteLine("What do you want to do");
-    Console.WriteLine("[S]ee all TODOS");
-    Console.WriteLine("[A]dd a TODO");
-    Console.WriteLine("[R]emove a TODO");
-    Console.WriteLine("[E]xit");
+    var result = new List<int>();
 
-
-    string userType = Console.ReadLine();
-  
-
-    switch (userType)
+    foreach(var number in numbers)
     {
-        case "E":
-        case "e":
-            Console.WriteLine("Exit");
-            shallExit = true;
-            break;
-
-        case "A":
-        case "a":
-            bool isDescriptionValid = false;
-            while (!isDescriptionValid)
-            {
-                Console.WriteLine("Add a todo");
-                var description = Console.ReadLine();
-                if (description == "")
-                {
-                    Console.WriteLine("User input cannot be empty");
-                }
-                else if (todos.Contains(description))
-                {
-                    Console.WriteLine("description must be unique");
-                }
-                else
-                {
-                    isDescriptionValid = true;
-                    todos.Add(description);
-                }
-            }
-            break;
-
-        case "S":
-        case "s":
-            for (var i = 0; i < todos.Count; ++i)
-            {
-                Console.WriteLine(todos[i]);
-            }
-            break;
-
-        case "R":
-        case "r":
-
-            var remove = Console.ReadLine();
-            for (var i = 0; i < todos.Count; ++i)
-            {
-                remove = todos[i];
-                if (!todos.Contains(remove))
-                {
-                    Console.WriteLine("we can't remove what does not exist");
-                }
-                else
-                {
-                    todos.Remove(remove);
-                }
-            }
-            break;
-
-        default:
-            Console.WriteLine("Invalid Choice");
-            break;
-
+        if (number > 0)
+        {
+            result.Add(number);
+        }
     }
+    return result;
+}
+
+var positiveNumber = GetPositiveNumbers(numbers);
+
+foreach(var number in positiveNumber)
+{
+    Console.WriteLine(number);
 }
 
 Console.ReadKey();
