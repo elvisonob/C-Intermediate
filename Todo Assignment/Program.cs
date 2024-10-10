77623158b38b6988fddf1da5e59b33b0273070c2
elvisonob <elvisonob@yahoo.com>
2024-10-10 19:29:07 +0100

    See all todos and add a todo implemented

--- Todo Assignment/Program.cs
+++ Todo Assignment/Program.cs
@@ -1,36 +1,82 @@
 ﻿using System.ComponentModel.Design;
+using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.InteropServices;
 
+string userInput;
 
+List<string> todoList = new List<string>() { "Become a programming Genius", "Be the best dad ever" };
 
-//check the user input and return true if it is a number and return false when not a number
-
-// ask the user to keep writing a number until they give a valid input
-
-// so, if number is negative, user should keep making inputs until there is a positive.
-
-bool isParsingSuccessful;
 do
 {
-    Console.WriteLine("Enter a number:");
-    var userInput = Console.ReadLine();
-    isParsingSuccessful = int.TryParse(userInput, out int number);
+    Console.WriteLine("Hello!");
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
 
-    if (isParsingSuccessful)
+    userInput = Console.ReadLine();
+
+
+    // if an option that is not S, A, R, is selected, the user should be told that it is an incorrect choice
+
+    switch (userInput)
     {
-        Console.WriteLine("Number is valid " + number);
-    }
-    else
+        case "S":
+        case "s":
+            Console.WriteLine("See all Todos");
+            seeAllTodos();
+            break;
+        case "A":
+        case "a":
+            Console.WriteLine("Add a Todo");
+            
+            addATodo();
+            break;
+        case "R":
+        case "r":
+            Console.WriteLine("Remove a Todo");
+            break;
+        case "E":
+        case "e":
+            Console.WriteLine("Exit Loop");
+            break;
+        default:
+            Console.WriteLine("Invalid Input");
+            break;
+    } 
+} while (userInput != "E");
+
+//Writing a method for add a todo, it includes the userInput to the List array
+
+void addATodo(){
+    //if case A is selected, the user Input should be collected afterwards and added to list
+    var newInput = Console.ReadLine();
+    todoList.Add(newInput);
+}
+
+void seeAllTodos()
+{
+    for (int i = 0; i < todoList.Count; ++i)
     {
-        Console.WriteLine("Number is not valid");
+        Console.WriteLine($"{i + 1}: " + todoList[i] );
     }
+}
+
+void removeATodo(){
+    Console.WriteLine("Select the index of the todo you want to remove");
+}
 
 
-} while (!isParsingSuccessful);
 
 
 
 
-Console.ReadKey();
+
+
+
+
+
 
 
 
